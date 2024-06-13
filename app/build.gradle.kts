1249ae952e51286e57728bf889620ffac47f3329
Quique <enriquegcameselle@gmail.com>
2024-06-13 13:53:21 +0200

    implemented all needed libraries except for room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+
+    //daggerHilt
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,10 +40,22 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    val navCompVersion = "2.7.7"
+
+    //navComponent
+    implementation("androidx.navigation:navigation-fragment-ktx:$navCompVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navCompVersion")
+
+
+    //common
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +64,34 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Lifecycle & Coroutines
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-simplexml:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:compiler:4.16.0")
+    ksp("com.github.bumptech.glide:ksp:4.16.0")
+    implementation("com.caverock:androidsvg:1.4")
+
+    //daggerHilt
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+}
+
+//necesito el kapt para dagger, parece
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    //para pasar parámetros a través del navController
+    id("androidx.navigation.safeargs.kotlin") version "2.7.1" apply false
+    //ksp
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+
+    //dagger Hilt
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
