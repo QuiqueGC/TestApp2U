a7d05267e99339f5257d06dece7e1718eac5748e
Quique <enriquegcameselle@gmail.com>
2024-06-13 13:47:38 +0200

    added Fragments

--- app/src/main/java/com/example/testapp2u/ui/MainActivity.kt
+++ app/src/main/java/com/example/testapp2u/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.testapp2u
+package com.example.testapp2u.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.testapp2u.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
