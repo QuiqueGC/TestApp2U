f1444d5b7e0546d040a46762b0e6b64ffee40bbc
Quique <enriquegcameselle@gmail.com>
2024-06-13 17:34:25 +0200

    all the components added to the fragment_list.xml

--- app/src/main/java/com/example/testapp2u/ui/MainActivity.kt
+++ app/src/main/java/com/example/testapp2u/ui/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.testapp2u.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.testapp2u.R
 import com.example.testapp2u.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +9,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
     }
