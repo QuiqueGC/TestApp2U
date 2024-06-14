ddb172d37274d8db222de3d17bd69728b80140d5
Quique <enriquegcameselle@gmail.com>
2024-06-14 12:44:18 +0200

    added OkHttpClient to validate access to the API

--- app/src/main/java/com/example/testapp2u/di/NetworkModule.kt
+++ app/src/main/java/com/example/testapp2u/di/NetworkModule.kt
@@ -7,6 +7,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,12 +19,33 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val requestBuilder = original.newBuilder()
+                    .header(
+                        "Authorization",
+                        Credentials.basic(
+                            MyConstants.USER_NAME_API_CALL,
+                            MyConstants.PASSWD_API_CALL
+                        )
+                    )
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         val gson = GsonBuilder().setLenient().create()
 
         return Retrofit.Builder()
             .baseUrl(MyConstants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
             .build()
     }
 
