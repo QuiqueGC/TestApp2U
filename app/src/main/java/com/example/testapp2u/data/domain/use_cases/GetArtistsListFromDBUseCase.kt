141ed99644a9d38d26cf3094528f87149b8afa31
Quique <enriquegcameselle@gmail.com>
2024-06-17 14:40:35 +0200

    fixed a little mistake with use cases

--- app/src/main/java/com/example/testapp2u/data/domain/use_cases/GetArtistsListFromDBUseCase.kt
+++ app/src/main/java/com/example/testapp2u/data/domain/use_cases/GetArtistsListFromDBUseCase.kt
@@ -1,15 +1,14 @@
 package com.example.testapp2u.data.domain.use_cases
 
 import android.util.Log
+import com.example.testapp2u.data.domain.models.artist.ArtistModel
 import com.example.testapp2u.data.domain.repository.DataProvider
-import com.example.testapp2u.data.domain.repository.db.entities.artist.ArtistEntity
-import com.example.testapp2u.data.domain.repository.remote.response.BaseResponse
 import javax.inject.Inject
 
 class GetArtistsListFromDBUseCase @Inject constructor(private val dataProvider: DataProvider) {
 
-    suspend operator fun invoke(limit: Int, offset: Int): BaseResponse<List<ArtistEntity>> {
+    suspend operator fun invoke(): List<ArtistModel> {
         Log.i("apiCall", "entró en el caso de uso")
-        return dataProvider.getArtistsListFromRemote(limit, offset)
+        return dataProvider.getArtistsFromDB()
     }
 }
--- app/src/main/java/com/example/testapp2u/data/domain/use_cases/GetArtistsListFromRemoteUseCase.kt
+++ app/src/main/java/com/example/testapp2u/data/domain/use_cases/GetArtistsListFromRemoteUseCase.kt
@@ -1,14 +1,15 @@
 package com.example.testapp2u.data.domain.use_cases
 
 import android.util.Log
-import com.example.testapp2u.data.domain.models.artist.ArtistModel
 import com.example.testapp2u.data.domain.repository.DataProvider
+import com.example.testapp2u.data.domain.repository.db.entities.artist.ArtistEntity
+import com.example.testapp2u.data.domain.repository.remote.response.BaseResponse
 import javax.inject.Inject
 
 class GetArtistsListFromRemoteUseCase @Inject constructor(private val dataProvider: DataProvider) {
 
-    suspend operator fun invoke(limit: Int, offset: Int): List<ArtistModel> {
+    suspend operator fun invoke(limit: Int, offset: Int): BaseResponse<List<ArtistEntity>> {
         Log.i("apiCall", "entró en el caso de uso")
-        return dataProvider.getArtistsFromDB()
+        return dataProvider.getArtistsListFromRemote(limit, offset)
     }
 }
